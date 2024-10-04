4b456384a70adf51e72b3dc1b2747a64afc59f95
Alexius Maliuk Dias <alexiusmd2004@gmail.com>
2024-10-04 18:39:48 -0300

    Ayo the pizza here 🍕

--- InstanciaBZ.py
+++ InstanciaBZ.py
@@ -10,30 +10,27 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from Ponto import *
 import random
+import math
 
-""" Classe Instancia """
 class InstanciaBZ:   
     def __init__(self, curva):
-        self.posicao = Ponto (0,0,0) 
-        self.escala = Ponto (1,1,1)
-        self.rotacao:float = 0.0
+        self.posicao = Ponto(0, 0, 0)
+        self.escala = Ponto(1, 1, 1)
+        self.rotacao: float = 0.0
         self.modelo = None
         self.t = 0.0
         self.curva = curva
         self.entry_at_line_start = True
+        self.is_moving_forward = True
         self.previous_movement_direction = None
         self.next_curve = curva
         self.is_next_curve_set = False
         self.inverted = False
-    
-    """ Imprime os valores de cada eixo do ponto """
-    # Faz a impressao usando sobrecarga de funcao
-    # https://www.educative.io/edpresso/what-is-method-overloading-in-python
+
     def imprime(self, msg=None):
         if msg is not None:
             print(msg)
 
-    """ Define o modelo a ser usada para a desenhar """
     def setModelo(self, func):
         self.modelo = func
 
@@ -42,31 +39,32 @@ class InstanciaBZ:
             self.posicao = self.curva.Calcula(self.t)
 
     def moveOnCurve(self, is_moving_forward):
+        self.is_moving_forward = is_moving_forward
         if self.previous_movement_direction != is_moving_forward:
             self.is_next_curve_set = False
 
         self.previous_movement_direction = is_moving_forward
 
         if not (is_moving_forward ^ self.entry_at_line_start):
-            self.increaseT(is_moving_forward)
+            self.increaseT()
         else:
-            self.decreaseT(is_moving_forward)
+            self.decreaseT()
 
         self.rotacao = self.calculateRotation()
 
-    def increaseT(self, is_moving_forward):
+    def increaseT(self):
         self.t += 0.05
         if self.t >= 0.5 and not self.is_next_curve_set:
-            self.next_curve = self.selectCurves(is_moving_forward)
+            self.next_curve = self.selectCurves()
         if self.t > 1:
             self.t = 1
             self.switchCurve()
         self.update_position()
 
-    def decreaseT(self, is_moving_forward):
+    def decreaseT(self):
         self.t -= 0.05
         if self.t <= 0.5 and not self.is_next_curve_set:
-            self.next_curve = self.selectCurves(is_moving_forward)
+            self.next_curve = self.selectCurves()
         if self.t < 0:
             self.t = 0
             self.switchCurve()
@@ -77,11 +75,10 @@ class InstanciaBZ:
         self.entry_at_line_start = not self.entry_at_line_start
         self.curva = self.next_curve
 
-    def selectCurves(self, is_moving_forward):
+    def selectCurves(self):
         self.is_next_curve_set = True
-        if not (self.entry_at_line_start ^ is_moving_forward):
+        if not (self.entry_at_line_start ^ self.is_moving_forward):
             return random.choice(self.curva.getAdjacentCurvesAtEnd())
-        
         return random.choice(self.curva.getAdjacentCurvesAtStart())
 
     def calculateTangent(self):
@@ -95,9 +92,9 @@ class InstanciaBZ:
     def calculateRotation(self):
         tangent = self.calculateTangent()
         angle = math.atan2(tangent.y, tangent.x)
-
-        return math.degrees(angle) - 90
-
+        if not (self.entry_at_line_start ^ self.is_moving_forward):
+            return math.degrees(angle) - 90
+        return math.degrees(angle) + 90
 
     def Desenha(self):
         self.rotacao = self.calculateRotation()
@@ -107,5 +104,4 @@ class InstanciaBZ:
         glRotatef(self.rotacao, 0, 0, 1)
         glScalef(self.escala.x, self.escala.y, self.escala.z)
         self.modelo()
-        
         glPopMatrix()
